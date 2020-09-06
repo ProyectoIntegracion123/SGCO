f65f3f1b385dc053e08d692f110b39f6db19663d
Proyecto-DAD <Proyecto.DAD@outlook.es>
2020-09-05 20:17:44 -0500

    agregue clase conexion

---
+++ CapaAccesoDatos/Conexion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CapaAccesoDatos
+{
+    public class Conexion
+    {
+        #region "PATRON SINGLETON"
+        private static Conexion conexion = null;
+        private Conexion() { }
+        public static Conexion getInstance()
+        {
+            if (conexion == null)
+            {
+                conexion = new Conexion();
+            }
+            return conexion;
+        }
+        #endregion
+
+        public SqlConnection ConexionDB()
+        {
+            SqlConnection conexion = new SqlConnection();
+            conexion.ConnectionString = @"Server=rodrinchelo.database.windows.net;Database=ClinicaOdontologica;User Id=UserClinica;Password=Clinica123;";
+            return conexion;
+        }
+
+    }
+}
